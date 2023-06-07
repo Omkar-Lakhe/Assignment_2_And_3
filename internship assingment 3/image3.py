47e6649e611a728e5822bc5468d2b656908e04bf
Omkar-Lakhe <104200749+Omkar-Lakhe@users.noreply.github.com>
2023-06-07 17:12:55 +0530

    .

---
+++ internship assingment 3/image3.py
@@ -0,0 +1,71 @@
+from PIL import Image 
+import cv2
+  
+# Opens a image in RGB mode 
+im = Image.open('try3.jpg')
+img = cv2.imread('try3.jpg')
+print(img.shape)
+
+  
+# Setting the points for cropped image 
+left = 290
+top = 290
+right = 580
+bottom = 380
+  
+# Cropped image of above dimension 
+# (It will not change orginal image) 
+im1 = im.crop((left, top, right, bottom)) 
+  
+# Shows the image in image viewer 
+im1.save('Data\main3-1.jpg')
+Image.open('Data\main3-1.jpg')
+
+
+
+#code for segmentation
+import cv2
+from PIL import Image 
+from pytesseract import image_to_string 
+
+#img = Image.open("/Users/KHUSH/Downloads/segmented.jpg") 
+img = cv2.imread('Data\main3-1.jpg')
+mser = cv2.MSER_create()
+
+#Resize the image so that MSER can work better
+img = cv2.resize(img, (img.shape[1]*2, img.shape[0]*2))
+img = img[5:-5,5:-5,:]
+
+gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+vis = img.copy()
+
+regions = mser.detectRegions(gray)
+
+hulls = [cv2.convexHull(p.reshape(-1, 1, 2)) for p in regions[0]]
+cv2.polylines(vis, hulls, 1, (0,255,0)) 
+
+cv2.imwrite("Data\main3-2.jpg",img)
+cv2.namedWindow('img', 0)
+#print(image_to_string(Image.open('/Users/KHUSH/Downloads/segmented2.jpg'),lang='eng'))
+cv2.imshow('img', vis)
+while(cv2.waitKey()!=ord('q')):
+       continue
+cv2.destroyAllWindows()
+
+
+
+try:
+    from PIL import Image
+except ImportError:
+    import Image
+import pytesseract
+img = Image.open('try3.jpg') 
+
+def ocr_core(img):
+    """
+    This function will handle the core OCR processing of images.
+    """
+    text = pytesseract.image_to_string(Image.open(img))  # We'll use Pillow's Image class to open the image and pytesseract to detect the string in the image
+    return text
+
+print(ocr_core('try3.jpg'))
---
+++ internship assingment 3/image4.py
@@ -0,0 +1,73 @@
+from PIL import Image 
+import cv2
+  
+# Opens a image in RGB mode 
+im = Image.open('try4.jpg')
+
+#To find the x and y co-ordinates of the image
+img = cv2.imread('try4.jpg')
+print(img.shape)
+
+  
+# Setting the points for cropped image 
+left = 350
+top = 160
+right = 1220
+bottom = 500
+  
+# Cropped image of above dimension 
+# (It will not change orginal image) 
+im1 = im.crop((left, top, right, bottom)) 
+  
+# Shows the image in image viewer 
+im1.save('Data\main4-1.jpg')
+Image.open('Data\main4-1.jpg')
+
+
+
+#code for segmentation
+import cv2
+from PIL import Image 
+from pytesseract import image_to_string 
+
+#img = Image.open("/Users/KHUSH/Downloads/segmented.jpg") 
+img = cv2.imread('Data\main4-1.jpg')
+mser = cv2.MSER_create()
+
+#Resize the image so that MSER can work better
+img = cv2.resize(img, (img.shape[1]*2, img.shape[0]*2))
+img = img[5:-5,5:-5,:]
+
+gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+vis = img.copy()
+
+regions = mser.detectRegions(gray)
+
+hulls = [cv2.convexHull(p.reshape(-1, 1, 2)) for p in regions[0]]
+cv2.polylines(vis, hulls, 1, (0,255,0)) 
+
+cv2.imwrite("Data\main4-2.jpg",img)
+cv2.namedWindow('img', 0)
+#print(image_to_string(Image.open('/Users/KHUSH/Downloads/segmented2.jpg'),lang='eng'))
+cv2.imshow('img', vis)
+while(cv2.waitKey()!=ord('q')):
+       continue
+cv2.destroyAllWindows()
+
+
+
+try:
+    from PIL import Image
+except ImportError:
+    import Image
+import pytesseract
+img = Image.open('try4.jpg') 
+
+def ocr_core(img):
+    """
+    This function will handle the core OCR processing of images.
+    """
+    text = pytesseract.image_to_string(Image.open(img))  # We'll use Pillow's Image class to open the image and pytesseract to detect the string in the image
+    return text
+
+print(ocr_core('try4.jpg'))
